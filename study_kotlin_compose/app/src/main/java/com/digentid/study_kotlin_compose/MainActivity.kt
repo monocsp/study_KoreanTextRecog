335f08f328056047d10938a3eaec509d2bbb3b92
monocsp <monocsp@naver.com>
2024-01-03 16:56:39 +0900

    [update] kotlin compose

--- study_kotlin_compose/app/src/main/java/com/digentid/study_kotlin_compose/MainActivity.kt
+++ study_kotlin_compose/app/src/main/java/com/digentid/study_kotlin_compose/MainActivity.kt
@@ -1,48 +1,64 @@
-package com.digentid.cameratextreco
+package com.digentid.study_kotlin_compose
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.digentid.cameratextreco.ui.theme.CameraTextRecoTheme
-import com.google.mlkit.vision.common.InputImage
-import com.google.mlkit.vision.text.TextRecognition
-import com.google.mlkit.vision.text.korean.KoreanTextRecognizerOptions
-import java.io.IOException
+import com.digentid.study_kotlin_compose.ui.theme.Study_kotlin_composeTheme
+import java.io.File
+import java.util.concurrent.ExecutorService
 
 class MainActivity : ComponentActivity() {
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExcutor: ExecutorService
+
+    private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CameraTextRecoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            if (shouldShowCamera.value) {
+                CameraView(
+                    outputDirectory = outputDirectory,
+                    executor = cameraExcutor,
+                    onImageCaptured = ::handleImageCapture,
+                    onError = { Log.e("TAG", "VIEW ERROR ", it) })
             }
+
+//            Study_kotlin_composeTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
         }
         requestCameraPermission()
     }
 
+    private fun handleImageCapture(uri: Uri) {
+        Log.i("TAG", "IMAGE CAPTURE : $uri")
+
+        shouldShowCamera.value = false
+    }
 
     private fun requestCameraPermission() {
         when {
@@ -73,38 +89,29 @@ class MainActivity : ComponentActivity() {
             }
         }
 
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()
+            ?.let { File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+    }
 
-}
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExcutor.shutdown()
+    }
 
-
-fun showCaptureImage(context: Context, img: Uri) {
-
-    val image = InputImage.fromFilePath(context, img)
-    val recognizer = TextRecognition.getClient(KoreanTextRecognizerOptions.Builder().build())
-    recognizer.process(image)
-        .addOnSuccessListener {
-            Log.d("TAG", "ShowCaptureImage: " + it.text)
-        }
-
-
-        .addOnFailureListener {
-            Log.d("TAG", "ShowCaptureImage: " + it.message)
-        }
 }
 
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello  $name!")
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    CameraTextRecoTheme {
+    Study_kotlin_composeTheme {
         Greeting("Android")
     }
 }
-
-
-

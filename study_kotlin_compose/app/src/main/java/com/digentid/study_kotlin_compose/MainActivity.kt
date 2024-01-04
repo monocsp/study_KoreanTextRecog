ea8402a612c698be6365bebe1bacfab91a1df6a8
monocsp <monocsp@naver.com>
2024-01-04 16:42:18 +0900

    [update] korean text rec screen

--- study_kotlin_compose/app/src/main/java/com/digentid/study_kotlin_compose/MainActivity.kt
+++ study_kotlin_compose/app/src/main/java/com/digentid/study_kotlin_compose/MainActivity.kt
@@ -8,27 +8,38 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.digentid.study_kotlin_compose.ui.theme.Study_kotlin_composeTheme
+
 import java.io.File
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import coil.compose.rememberImagePainter
 
 class MainActivity : ComponentActivity() {
     private lateinit var outputDirectory: File
     private lateinit var cameraExcutor: ExecutorService
 
     private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
-
+    private lateinit var photoUri: Uri
+    private var shouldShowPhoto: MutableState<Boolean> = mutableStateOf(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,25 +50,38 @@ class MainActivity : ComponentActivity() {
                     executor = cameraExcutor,
                     onImageCaptured = ::handleImageCapture,
                     onError = { Log.e("TAG", "VIEW ERROR ", it) })
+            } else {
+                CameraView(
+                    outputDirectory = outputDirectory,
+                    executor = cameraExcutor,
+                    onImageCaptured = ::handleImageCapture,
+                    onError = { Log.e("TAG", "VIEW ERROR ", it) })
+
             }
 
-//            Study_kotlin_composeTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
+            if (shouldShowPhoto.value) {
+                Image(
+                    painter = rememberImagePainter(photoUri),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+
         }
+
+
         requestCameraPermission()
+        outputDirectory = getOutputDirectory()
+        cameraExcutor = Executors.newSingleThreadExecutor()
     }
 
     private fun handleImageCapture(uri: Uri) {
         Log.i("TAG", "IMAGE CAPTURE : $uri")
 
         shouldShowCamera.value = false
+        photoUri = uri
+        shouldShowPhoto.value = true
     }
 
     private fun requestCameraPermission() {
@@ -67,11 +91,14 @@ class MainActivity : ComponentActivity() {
                 Manifest.permission.CAMERA
             ) == PackageManager.PERMISSION_GRANTED -> {
                 Log.i("TAG", "Permission Previously granted")
+                shouldShowCamera.value = true
             }
+
             ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
                 Manifest.permission.CAMERA
             ) -> Log.i("TAG", "SHOW CAMERA PERMISSION DIALOG")
+
             else -> requestPermissionLauncher.launch(Manifest.permission.CAMERA)
 
         }
@@ -81,7 +108,7 @@ class MainActivity : ComponentActivity() {
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
             if (isGranted) {
                 Log.i("TAG", "PERMISSION GRANT")
-
+                shouldShowCamera.value = true
 
             } else {
                 Log.i("TAG", "PERMISSION DENIED")
@@ -103,15 +130,46 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Study_kotlin_composeTheme {
-        Greeting("Android")
+    ScaffoldExample()
+
+}
+
+
+@Composable
+fun ScaffoldExample() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+
+                title = {
+                    Text("Top app bar")
+                }
+            )
+        },
+
+
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                """
+                    This is an example of a scaffold. It uses the Scaffold composable's parameters to create a screen with a simple top app bar, bottom app bar, and floating action button.
+
+                    It also contains some basic inner content, such as this text.
+
+                    
+                """.trimIndent(),
+            )
+        }
     }
 }
